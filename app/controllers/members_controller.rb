9464f88941bc223aacbe28d86d2b30ec2249cef6
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-25 18:52:00 -0500

    members controller working

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,29 +2,30 @@ class MembersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    user = get_user_from_token
+    user = User.find_by(name: params[:name])
     if user
-    render json: {
-      message: 'You are in',
-      user: user
-    }
-  end
-  render json: {
-    message: 'Authentication failed',
-  }, status: :unauthorized
-  end
-
-  private
-
-  def get_user_from_token
-    auth = request.headers['Authorization']
-    if auth
-    jwt_payload = JWT.decode(auth.split(' ')[1],
-                            Rails.application.credentials.devise[:jwt_secret_key]).first
-    user_id = jwt_payload['sub']
-    user = User.find(user_id.to_s)
+      render json: {
+        message: 'You are in',
+        user: user
+      }
     else
-      nil
+      render json: {
+        message: 'Authentication failed',
+      }, status: :unauthorized
     end
   end
+
+  # private
+
+  # def get_user_from_token
+  #   auth = request.headers['Authorization']
+  #   if auth
+  #     jwt_payload = JWT.decode(auth.split(' ')[1],
+  #                           Rails.application.credentials.devise[:jwt_secret_key]).first
+  #     user_id = jwt_payload['sub']
+  #     user = User.find(user_id.to_s)
+  #   else
+  #     nil
+  #   end
+  # end
 end
