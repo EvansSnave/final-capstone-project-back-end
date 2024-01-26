e9cd9445a01a388a77198cdcff8119b2e50a3dfb
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-26 12:22:25 -0500

    fix linter errors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :verify_authenticity_token, raise: false
   before_action :authenticate_devise_api_token!, only: [:restricted]
-  def home
-  end
+  def home; end
 
   def restricted
     devise_api_token = current_devise_api_token
-    if devise_api_token 
+    if devise_api_token
       render json: {
         message: "You are logged in as #{devise_api_token.resource_owner.email}"
       }, status: :ok
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Pages", type: :request do
+RSpec.describe 'Pages', type: :request do
   params = {
     email: 'kevin@email.com',
     password: 'password',
@@ -8,25 +8,25 @@ RSpec.describe "Pages", type: :request do
   }
 
   before do
-    post '/users/tokens/sign_up', as: :json, params: params
+    post '/users/tokens/sign_up', as: :json, params:
   end
 
-  describe "GET /home" do
-    it "returns http success" do
-      get "/pages/home"
+  describe 'GET /home' do
+    it 'returns http success' do
+      get '/pages/home'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "registrations" do
-    it "validates new user registrations" do
+  describe 'registrations' do
+    it 'validates new user registrations' do
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "login system" do
-    it "validates user login" do
-      post '/users/tokens/sign_in', as: :json, params: params
+  describe 'login system' do
+    it 'validates user login' do
+      post('/users/tokens/sign_in', as: :json, params:)
       expect(response).to have_http_status(:success)
       response_json = JSON.parse(response.body)
       expect(response_json).to have_key('resource_owner')
@@ -34,5 +34,4 @@ RSpec.describe "Pages", type: :request do
       expect(resource_owner_email).to eq('kevin@email.com')
     end
   end
-
 end
