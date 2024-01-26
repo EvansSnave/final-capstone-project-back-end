ae74d1512e9724ee5fb9cf6f5803f03966ed5788
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-26 12:21:53 -0500

    add tests

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
+  params = {
+    email: 'kevin@email.com',
+    password: 'password',
+    password_confirmation: 'password'
+  }
+
+  before do
+    post '/users/tokens/sign_up', as: :json, params: params
+  end
+
   describe "GET /home" do
     it "returns http success" do
       get "/pages/home"
@@ -8,10 +18,20 @@ RSpec.describe "Pages", type: :request do
     end
   end
 
-  describe "GET /restricted" do
-    it "returns http success" do
-      get "/pages/restricted"
+  describe "registrations" do
+    it "validates new user registrations" do
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "login system" do
+    it "validates user login" do
+      post '/users/tokens/sign_in', as: :json, params: params
+      expect(response).to have_http_status(:success)
+      response_json = JSON.parse(response.body)
+      expect(response_json).to have_key('resource_owner')
+      resource_owner_email = response_json['resource_owner']['email']
+      expect(resource_owner_email).to eq('kevin@email.com')
     end
   end
 
