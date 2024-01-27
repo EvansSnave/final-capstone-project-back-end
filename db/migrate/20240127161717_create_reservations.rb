864a80909dae473f39501ba037f4a48e8aee0a26
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-27 14:56:35 -0500

    add cities

--- db/migrate/20240127161717_create_reservations.rb
+++ db/migrate/20240127161717_create_reservations.rb
@@ -2,22 +2,12 @@ class CreateReservations < ActiveRecord::Migration[7.1]
   def change
     create_table :reservations do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :doctor, null: false, foreign_key: true
       t.references :city, null: false, foreign_key: true
       t.date :schedule_date
 
-      validates_presence_of :user, :doctor, :schedule_date, :city
-      validate :schedule_date_in_future
-
-      private
-
-      def schedule_date_in_future
-        if schedule_date.present? && schedule_date <= Date.today
-          errors.add(:schedule_date, "must be in the future")
-        end
-      end
-
       t.timestamps
     end
+
+    add_index :reservations, [:user_id, :city_id, :schedule_date], unique: true, name: 'unique_reservation_index'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_26_231242) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_27_161717) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "city_id", null: false
+    t.date "schedule_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_reservations_on_city_id"
+    t.index ["user_id", "city_id", "schedule_date"], name: "unique_reservation_index", unique: true
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -29,4 +46,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_26_231242) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "cities"
+  add_foreign_key "reservations", "users"
 end
