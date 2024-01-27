a4b356e15db57655d0ccd9f9a256f2715f335e3f
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-26 19:30:51 -0500

    update serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,7 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :created_at, :name
+  attributes :id, :email, :name
 
-  attribute :created_date do |user|
-    user.created_at && user.created_at.strftime('%m/%d/%Y')
-  end
   attribute :name do |user|
     user.name && user.name
   end
