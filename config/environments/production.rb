1a2b76063667289d7fd8bc22a9bd9d6d30b19d96
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-29 00:11:33 -0500

    update production environtment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.require_master_key = true
 end
