a8be3003df19af64feced5d9425812d959d90261
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-03 09:02:11 +0100

    Update the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::API
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name username])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name username])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
   end
 end
