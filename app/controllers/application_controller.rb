fb503cd3f71802150d8d1368a8db91599429ede9
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-07 12:29:21 +0100

    Fix the permit parameter in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name username])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name username email])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name username email])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
   end
 end
