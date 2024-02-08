e20f2029d3372a45652d717eb84d1e8f84b56a36
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-08 06:11:12 +0100

    Add unit test for the details controllers

--- spec/requests/details_spec.rb
+++ spec/requests/details_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe 'Details', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe DetailsController, type: :controller do
+  let(:user) { create(:user) }
+  let(:city) { create(:city) }
+  let(:doctor) { create(:doctor, user:, city:) }
+  let(:detail) { create(:detail, doctor:) }
+
+  describe 'GET #show' do
+    before do
+      get :show, params: { id: detail.id }, format: :json
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'JSON body response contains expected detail attributes' do
+      json_response = JSON.parse(response.body)
+      expect(json_response.keys).to match_array(%w[id price specialization studies doctor_id created_at
+                                                   updated_at])
+    end
+
+    it 'responds with the correct detail' do
+      expect(response.body).to eq(detail.to_json)
+    end
   end
 end
