574f02747abbea8f8d11c32662c775e1d0de9dff
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-08 11:25:33 +0100

    Add content security policy middleware

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module FinalCapstoneProjectBackEnd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use ActionDispatch::ContentSecurityPolicy::Middleware
   end
 end
