c9e069ba628593e93989b404a2005c7b5209c1f9
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-08 17:44:36 +0100

    Update Api documentation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,8 @@
 class Doctor < ApplicationRecord
   belongs_to :user
   belongs_to :city
+  has_one :detail, dependent: :destroy
+  has_many :reservations
 
   accepts_nested_attributes_for :detail
 end
---
+++ spec/requests/users/registrations_spec.rb
@@ -0,0 +1,36 @@
+require 'swagger_helper'
+
+RSpec.describe 'users/registrations', type: :request do
+  path '/signup' do
+    post('Create a new user') do
+      tags 'Users'
+      response(200, 'successful') do
+        consumes 'application/json'
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            user: {
+              type: :object,
+              properties: {
+                name: { type: :string },
+                email: { type: :string, format: :email },
+                password: { type: :string, format: :password },
+                password_confirmation: { type: :string, format: :password }
+              }
+            }
+          },
+          required: %w[name email password]
+        }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
